292e5d7f63b05440b505f58ecc19e810022d197e
nsterling76 <nos5299@psu.edu>
2016-12-29 00:11:26 -0800

    Encapsulated MainMenu

--- src/credentialsapp/MainMenu.java
+++ src/credentialsapp/MainMenu.java
@@ -5,13 +5,11 @@
  */
 package credentialsapp;
 
-import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+
 import javax.swing.JPanel;
-import javax.swing.JSlider;
-import javax.swing.JTextField;
 
 /**
  *
