6486cefca4ce658422d3f9286351e79d949ba6f1
nsterling76 <nos5299@psu.edu>
2016-12-29 00:07:57 -0800

    Added a few to the main menu cntl

--- src/credentialsapp/MainMenuCntl.java
+++ src/credentialsapp/MainMenuCntl.java
@@ -5,10 +5,41 @@
  */
 package credentialsapp;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author nsterling76
  */
 public class MainMenuCntl {
+     private MainMenu menu;
     
+    MainMenuCntl(){
+    
+    }
+   
+    class Authenticate implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+           
+            JButton btu = (JButton) ae.getSource();
+            if () {
+            }
+
+            if (btu == view.getNewUSer()) {
+                view.dispose();
+                NewUser user = new NewUser(model);
+
+            }
+
+        }
+    }
 }
